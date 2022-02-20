dad07819d830f54b16fac7875aade6c7c08bce84
articlen <igor9os4a@gmail.com>
2022-02-20 13:46:35 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketsConfig) {  
+  let arr = [];
+  let reg;
+  let newStr = "[]^$.|?*+(){}<>";
+  for (let i = 0; i < bracketsConfig.length; i++) {
+      if (newStr.includes(bracketsConfig[i][0])) {
+          arr.push(`\\${bracketsConfig[i][0]}\\${bracketsConfig[i][1]}`);
+      } else {
+          arr.push(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`);
+      }
+      console.log(arr);
+      reg = new RegExp(arr.join("|"));
+      console.log(reg);
+  }
+  while (reg.test(str)) {
+      str = str.replace(reg, "");
+  }
+  return str.length === 0;
+};
